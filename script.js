a1f0058b4530b36e809b9994f1f830f18baf7788
akhileshsude <akhileshsude656@gmail.com>
2026-04-08 23:00:18 +0530

    adding sorting functions

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-let pg="https://api.rawg.io/api/games?key=68a0332c24494c5d9367221d470f397b"
-let nextpage
+let pg = "https://api.rawg.io/api/games?key=68a0332c24494c5d9367221d470f397b";
+let nextpage;
 
 const fetchGames = async (url) => {
   const res = await fetch(url);
-  const { results,next } = await res.json();
+  let { results, next } = await res.json();
   nextpage = next;
+  if (document.getElementById("sort").value === "ascending") {
+    document.getElementById("games-grid").innerHTML = "";
+    results = results.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (document.getElementById("sort").value === "descending") {
+    document.getElementById("games-grid").innerHTML = "";
 
-  console.log(results[0]);
+    results = results.sort((a, b) => b.name.localeCompare(a.name));
+  } else if (document.getElementById("sort").value === "highest") {
+    document.getElementById("games-grid").innerHTML = "";
+
+    results = results.sort((a, b) => b.rating - a.rating);
+  } else if (document.getElementById("sort").value === "lowest") {
+    document.getElementById("games-grid").innerHTML = "";
+
+    results = results.sort((a, b) => a.rating - b.rating);
+  }else if (document.getElementById("sort").value === "newest") {
+    document.getElementById("games-grid").innerHTML = "";
+
+    results = results.sort((a, b) => new Date(b.released) - new Date(a.released));
+  }else if (document.getElementById("sort").value === "oldest") {
+    document.getElementById("games-grid").innerHTML = "";
+
+    results = results.sort((a, b) => new Date(a.released) - new Date(b.released));
+  }
+
+  // console.log(results[0]);
+
   const gamesgrid = document.getElementById("games-grid");
 
-  let gamecard=(x)=>{
+  let gamecard = (x) => {
     return `<div class="game-card">
       <img src="${x.background_image}" alt="${x.name}">
       <h2>${x.name}</h2>
       <p>Rating: ${x.rating}</p>
     </div>
-    `
-  }
+    `;
+  };
   results.forEach((game) => {
     gamesgrid.innerHTML += gamecard(game);
   });
-  
 };
+
+  let sort = document.getElementById("sort");
+  sort.addEventListener("change", () => {
+    console.log(sort.value);
+    fetchGames(pg);
+  });
+
 
 fetchGames(pg);
 
@@ -30,8 +61,8 @@ window.addEventListener("scroll", () => {
     const loading = document.getElementById("loading");
     loading.innerHTML = "<h1>Loading More Games...</h1>";
     setTimeout(() => {
-    fetchGames(nextpage);
-    loading.innerHTML = "";
-
-  },2000);}
+      fetchGames(nextpage);
+      loading.innerHTML = "";
+    }, 2000);
+  }
 });
